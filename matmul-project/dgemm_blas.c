c35eae1c5c2fefb75ca8dfcb065cfca23019568a
Yiyang-Zhao <145488207+Yiyang-Zhao@users.noreply.github.com>
2024-10-01 19:21:59 -0400

    Add files via upload

---
+++ matmul-project/dgemm_blas.c
@@ -0,0 +1,19 @@
+#ifdef OSX_ACCELERATE
+#  include <Accelerate/Accelerate.h>
+#elif defined(__ICC) || defined(__INTEL_COMPILER)
+#  include <mkl_cblas.h>
+#elif defined(__GNUC__) || defined(__GNUG__)
+#  include <cblas.h>
+#endif
+
+const char* dgemm_desc = "System CBLAS dgemm.";
+
+void square_dgemm(const int M, 
+                  const double * restrict A, 
+		  const double * restrict B, 
+		  double * restrict C)
+{
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
+                M, M, M,
+                1.0, A, M, B, M, 0.0, C, M);
+}
